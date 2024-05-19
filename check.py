bc2ab6284667dfac62362f7fe0ae946559c6e181
chernyy_pg <chernyy_pg@outlook.com>
2024-05-19 11:37:12 +0300

    -

--- check.py
+++ check.py
@@ -3,25 +3,31 @@ import re
 
 
 def find_issues_by_wallets(wallets):
+    all_issues = 0
     for wallet in wallets:
         issues = get_issues(wallet)
         if issues is not None:
             print(f'{wallet} | Найдено {len(issues)} issues:')
             for issue in issues:
+                all_issues += 1
                 print(f"- #{issue['number']} {issue['title']}")
 
-                description_match = re.search(r'# Description\s+(.+)', issue['body'])
-                methodology_match = re.search(r'# Detailed Methodology & Walkthrough\s+(.+)', issue['body'])
+                try:
+                    description_match = re.search(r'# Description\s+(.+)', issue['body'])
+                    methodology_match = re.search(r'# Detailed Methodology & Walkthrough\s+(.+)', issue['body'])
 
-                if description_match:
-                    description = description_match.group(1).strip()
-                    print(f'Description: {description}')
+                    if description_match:
+                        description = description_match.group(1).strip()
+                        print(f'Description: {description}')
 
-                if methodology_match:
-                    methodology = methodology_match.group(1).strip()
-                    print(f'Methodology: {methodology}')
+                    if methodology_match:
+                        methodology = methodology_match.group(1).strip()
+                        print(f'Methodology: {methodology}')
+                except:
+                    pass
 
             print(f'__________________________________________')
+    print(f'Всего кошельков: {len(wallets)} | Всего репортов: {all_issues}')
 
 
 def main():
